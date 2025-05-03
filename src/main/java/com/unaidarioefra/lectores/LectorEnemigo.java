5f8e5103abe73b5af64350e8c13467662c96cdc4
unaicastillo <unaicastillocarrillo@gmail.com>
2025-05-03 15:41:31 +0200

    lector de enemigo hecho

--- src/main/java/com/unaidarioefra/lectores/LectorEnemigo.java
+++ src/main/java/com/unaidarioefra/lectores/LectorEnemigo.java
@@ -7,36 +7,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LectorEnemigo {
-    public static ArrayList leerCsv(String pathInput){
-        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
-        String archivoCsv = pathInput+"ArchivoEnemigos.csv"; 
-        HashMap<String, String> ArchivoEnemigoss ;
+    public static ArrayList<HashMap<String, Integer>> leerCsv() {
+        ArrayList<HashMap<String, Integer>> arrayList = new ArrayList<>();
+        String archivoCsv = "ArchivoEnemigos.csv";
+        HashMap<String, Integer> archivoEnemigos;
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCsv))) {
             String line;
-            String lineReader=br.readLine();
-            String[] encabezados = lineReader.split(","); 
-
+            String lineReader = br.readLine(); 
+            String[] encabezados = lineReader.split(",");
             while ((line = br.readLine()) != null) {
-                mapaCoches = new HashMap<>();
+                archivoEnemigos = new HashMap<>();
                 String[] valores = line.split(",");
-                
-                
                 for (int i = 0; i < encabezados.length; i++) {
-                    mapaCoches.put(encabezados[i], valores[i]); 
+                    try {
+                        archivoEnemigos.put(encabezados[i], Integer.parseInt(valores[i]));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Valor no numérico en columna '" + encabezados[i] + "': " + valores[i]);
+                        archivoEnemigos.put(encabezados[i], 0); 
+                    }
                 }
-                
-                
-                arrayList.add(mapaCoches);
-                
+                arrayList.add(archivoEnemigos);
             }
-
-            
-
-
         } catch (IOException e) {
             System.err.println("Error al leer el archivo CSV: " + e.getMessage());
         }
         return arrayList;
-
     }
 }
